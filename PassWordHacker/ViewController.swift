e02c4a72c69e66cf8d1e34af833ef17fb49c296b
長崎茉優 <nagasaki@nagasakinoMacBook-Pro.local>
2022-08-15 16:01:42 +0900

    all done

---
+++ PassWordHacker/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  PassWordHacker
+//
+//  Created by 長崎茉優 on 2022/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password:Int = 1234
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func start(){
+        for i in 0...9999{
+            
+            countLabel.text = String(i)
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            if i == password{
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password % 10)
+                    password = password / 10
+                }
+                
+                resultLabel1.text = String(digits[3])
+                resultLabel2.text = String(digits[2])
+                resultLabel3.text = String(digits[1])
+                resultLabel4.text = String(digits[0])
+            }
+        }
+        
+    }
+    
+    @IBAction func reset(){
+        password = 1234
+        countLabel.text = "「START」ボタンを押して解析開始"
+        
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+    }
+
+
+}
+
